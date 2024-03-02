12436185f62cd1fb9eb9ee1821e56143ced250a4
Anumol-dot <99953075+Anumol-dot@users.noreply.github.com>
2024-03-02 22:24:30 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { ResidentsComponent } from './components/residents/residents.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    ResidentsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
